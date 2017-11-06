84767589b9eb2f0a7a00165a1797057b3c56a865
hunter <hskempton@gmail.com>
2017-11-05 22:21:30 -0500

    added document . ready

--- wp-content/themes/hunterdotme/js/app.js
+++ wp-content/themes/hunterdotme/js/app.js
@@ -1,22 +1,24 @@
-$(function() {
-	function changeMenu () {
-		var curUrl = ($(location).attr('href'));
-		curPage = curUrl.substring(curUrl.indexOf('#') + 1, curUrl.length);
-		if (curPage.indexOf('/') > 0) {
-			curPage = curPage.substring(0, curPage.indexOf('/'));
+$( document ).ready(function() {
+	$(function() {
+		function changeMenu () {
+			var curUrl = ($(location).attr('href'));
+			curPage = curUrl.substring(curUrl.indexOf('#') + 1, curUrl.length);
+			if (curPage.indexOf('/') > 0) {
+				curPage = curPage.substring(0, curPage.indexOf('/'));
+			}
+			$("#menu li").css({"background-color": "rgba(157, 167, 183, 0.5)"});
+			$("." + curPage).css({"background-color": "rgba(157, 167, 183, 0.9)"});
+			// $("#" + curPage + "-page").fadeIn(500);
+			if (curPage === "Portfolio") {
+				$("#fp-nav").fadeOut(500);
+			} else {
+				$("#fp-nav").fadeIn(500);
+			}
 		}
-		$("#menu li").css({"background-color": "rgba(157, 167, 183, 0.5)"});
-		$("." + curPage).css({"background-color": "rgba(157, 167, 183, 0.9)"});
-		// $("#" + curPage + "-page").fadeIn(500);
-		if (curPage === "Portfolio") {
-			$("#fp-nav").fadeOut(500);
-		} else {
-			$("#fp-nav").fadeIn(500);
-		}
-	}
-	changeMenu();
+		changeMenu();
 
-	$(window).on('hashchange', function(e){
-		changeMenu();	
+		$(window).on('hashchange', function(e){
+			changeMenu();	
+		});
 	});
 });
