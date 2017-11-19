371a54e7b899a8e087801842c7ade9eacda941b1
hunter <hskempton@gmail.com>
2017-11-19 15:09:52 -0500

    added site map xml and title tag

--- wp-content/themes/hunterdotme/header.php
+++ wp-content/themes/hunterdotme/header.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<title><?php get_the_title() ?></title>
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/2.9.4/jquery.fullpage.css" />
 	<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/bootstrap.min.css">
   <script src="https://use.fontawesome.com/c65a787200.js"></script>
