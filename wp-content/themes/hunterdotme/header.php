762678f7bbe14855f2e0b799c84bc09a8045a0aa
hunter <hskempton@gmail.com>
2017-11-20 20:50:42 -0500

    added favicon

--- wp-content/themes/hunterdotme/header.php
+++ wp-content/themes/hunterdotme/header.php
@@ -2,6 +2,7 @@
 <html>
 <head>
 	<title><?php echo get_the_title() ?></title>
+	<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon.ico" />
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/2.9.4/jquery.fullpage.css" />
 	<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/bootstrap.min.css">
   <script src="https://use.fontawesome.com/c65a787200.js"></script>
