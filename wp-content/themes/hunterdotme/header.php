811c2222346438b43e1d5b9257622be949c9f229
hunter <hskempton@gmail.com>
2017-11-28 18:25:09 -0500

    Added google analytics tag

--- wp-content/themes/hunterdotme/header.php
+++ wp-content/themes/hunterdotme/header.php
@@ -1,6 +1,15 @@
 <!DOCTYPE html>
 <html>
 <head>
+		<!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-110310452-1"></script>
+	<script>
+	  window.dataLayer = window.dataLayer || [];
+	  function gtag(){dataLayer.push(arguments);}
+	  gtag('js', new Date());
+
+	  gtag('config', 'UA-110310452-1');
+	</script>
 	<title><?php echo get_the_title() ?></title>
 	<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon.ico" />
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/2.9.4/jquery.fullpage.css" />
